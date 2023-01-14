9af6622d1f3f6890deab08f3d5d4fadb3d0439ce
Sebastian <sebastianvazquez928@gmail.com>
2023-01-14 00:44:03 -0600

    deploy2

--- src/database.js
+++ src/database.js
@@ -2,8 +2,8 @@ const mongoose= require('mongoose');
 
 const MONGODB_URI=process.env.MONGODB_URI;
 
-
-mongoose.connect(MONGODB_URI,{
+//
+mongoose.connect(process.env.MONGODB_URI,{
 useUnifiedTopology:true,
 useNewUrlParser:true
 
